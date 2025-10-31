07491bf89db4e21e882a05cb9664f497360b534f
girish-kor <2021ci83d@sigce.edu.in>
2025-10-31 22:31:25 +0530

    feat: Dynamically read version from src/__init__.py in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
+import re
 from setuptools import setup, find_packages
+
+# Read the version from src/__init__.py
+with open("src/__init__.py", "r") as f:
+    version_match = re.search(r"^__version__\s*=\s*['\"]([^'\"]*)['\"]", f.read(), re.MULTILINE)
+    if version_match:
+        __version__ = version_match.group(1)
+    else:
+        __version__ = "0.0.0" # Fallback
+
 
 setup(
     name='chalbe',
-    version='0.1.0',
+    version=__version__, # Use the dynamically read version
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -17,10 +27,10 @@ setup(
         'replicate',
         'together',
         'boto3',
-
     ],
-    entry_points='''
-        [console_scripts]
-        chal=src.commands:cli
-    ''',
+    entry_points={
+        'console_scripts': [
+            'chal=src.commands:cli',
+        ],
+    },
 )
